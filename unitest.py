cae1dc633debd404847a91e409112987af7d7720
Benjabriel <gbvaldez81@gmail.com>
2022-09-26 18:59:09 -0500

    Ya esta lista la automatizacion

--- unitest.py
+++ unitest.py
@@ -1,6 +1,7 @@
 from ctypes import cast
 from lib2to3.pgen2 import driver
 from multiprocessing.connection import wait
+from operator import imod
 from sqlite3 import Time
 from timeit import repeat
 import unittest
@@ -15,8 +16,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ActionChains
+from pyunitreport import HTMLTestRunner
+import tkinter as tk
 
 class usando_unittest(unittest.TestCase):
+    #@classmethod
     def setUp(self) -> None:
         chrome_options = Options()
         chrome_options.add_argument("--disable-extensions")
@@ -31,13 +35,14 @@ class usando_unittest(unittest.TestCase):
     
     def test_buscar(self):
         #Variable de busqueda
-        Buscar="Mr Beast"
+        Buscar="TusCosmopolis"
         driver=self.driver
         pause= ActionChains(driver)
-        wait = WebDriverWait(driver,60)
+        wait = WebDriverWait(driver,10)
         #Abrimos la cancion de WARRIOS
-        driver.get("https://www.youtube.com/watch?v=o3W5ngVTtRE")
+        driver.get("https://youtu.be/MLuOPeklG0U")
         #Presionamos espacio para comenzar el video
+        time.sleep(3)
         pause.send_keys(Keys.SPACE).perform()
         #Abrimos otra ventana
         driver.execute_script("window.open('');")
@@ -67,39 +72,56 @@ class usando_unittest(unittest.TestCase):
         elemento= wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="tabsContent"]/tp-yt-paper-tab[2]')))
         elemento.click()
         urlactual= driver.current_url
+        for i in range(0,7):
+            driver.execute_script("window.scrollTo(0,"+str(i*800)+ ")")
+            time.sleep(3)
 
-        for i in range(0,20):
-            elemento= wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="items" and @class="style-scope ytd-grid-renderer"]')))
-            elemento= elemento.find_elements(By.TAG_NAME,"ytd-grid-video-renderer")
-            elemento[i].click()
-            elemento = wait.until(EC.visibility_of_element_located((By.TAG_NAME,'video')))
-            elemento = wait.until(EC.element_to_be_clickable((By.CLASS_NAME,'ytp-play-button')))
-            elemento.click()
-            time.sleep(2)
-            #time.sleep(3)
-            #pause.send_keys(Keys.SPACE).perform()
-            driver.execute_script("window.scrollTo(0,700)") 
-            #Entramos a la caja de texto
-            elemento= wait.until(EC.element_to_be_clickable((By.XPATH,"//*[@id='simple-box']")))
-            elemento= elemento.find_element(By.XPATH,"//*[@id='placeholder-area']")
-            elemento.click()
-            elemento= elemento.find_element(By.XPATH,"//*[@id='contenteditable-root']")
-            elemento.send_keys("GOOD" + str(i))
-            elemento = wait.until(EC.element_to_be_clickable((By.ID,"submit-button")))
-            elemento.click()
-            time.sleep(0.5)
-            driver.get(urlactual)
-            time.sleep(0.5)
+        elemento= wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="items" and @class="style-scope ytd-grid-renderer"]')))
+        elemento = elemento.find_elements(By.TAG_NAME,"ytd-grid-video-renderer")
+        
+        link_videos=[]
+        for i in elemento:
+            link= i.find_element(By.TAG_NAME,"a").get_attribute("href")
+            link_videos.append(link)
 
-
-        #print("Miramos si estamos pide correo")
-        #assert "No se encontro el elemento" not in driver.page_source
+        numero=0
+        for i in link_videos:
+            try:
+                driver.get(i)
+                elemento = wait.until(EC.visibility_of_element_located((By.TAG_NAME,'video')))
+                elemento = wait.until(EC.element_to_be_clickable((By.CLASS_NAME,'ytp-play-button')))
+                elemento.click()
+                time.sleep(2)
+                driver.execute_script("window.scrollTo(0,700)") 
+                #Entramos a la caja de texto
+                elemento= wait.until(EC.element_to_be_clickable((By.XPATH,"//*[@id='simple-box']")))
+                elemento= elemento.find_element(By.XPATH,"//*[@id='placeholder-area']")
+                elemento.click()
+                elemento= elemento.find_element(By.XPATH,"//*[@id='contenteditable-root']")
+                numero+=1
+                elemento.send_keys("Buen video gracias TuCosmopolis " + str(numero))
+                elemento = wait.until(EC.element_to_be_clickable((By.ID,"submit-button")))
+                elemento.click()
+                time.sleep(0.5)
+                if numero==60:
+                    break
+            except:
+                pass
+            
 
     
-
     def tearDown(self) -> None:
         self.driver.close()
+        #driver.switch_to.window(driver.window_handles[0])
+        #cls.driver.close()
 
-if __name__== "__main__":
-    unittest.main()
-a
+#if __name__== "__main__":
+ #   unittest.main(verbosity=2,testRunner=HTMLTestRunner(output='reportes',report_name='Botas'))
+
+Ventana= tk.Tk()
+Ventana.geometry("200x500")
+L_Correo= tk.Label(Ventana,text="Bienvenido, es necesario que ingrese su" + 
+    " correo y contraseña para realizar la probar el programa\nEste programa no guardara dichos datos")
+
+Correo= tk.Entry(Ventana)
+Correo.pack()
